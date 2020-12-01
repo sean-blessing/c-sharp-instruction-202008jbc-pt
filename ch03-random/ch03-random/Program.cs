04ca04aa46cae0a81156f6ba1dec376bf3e2b710
Sean Blessing <snblessing@gmail.com>
2020-11-30 21:43:07 -0500

    ch3 demos

---
+++ ch03-random/ch03-random/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ch03_random {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Generate random #s!");
+
+            //Random r = new Random();
+            // using 'var' type, not necessary, but this is a demo
+            var r = new Random();
+            // generate random # between 0 and 9.999
+            int n1 = r.Next(10);
+            Console.WriteLine(n1);
+            // die roll... random # between 1 and 6
+            int n2 = r.Next(6) + 1;
+            Console.WriteLine(n2);
+
+            // die roll 2... another way
+            int n3 = r.Next(1, 7);
+            Console.WriteLine(n3);
+        }
+    }
+}
---
+++ ch03-round-format/ch03-round-format/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ch03_round_format {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Rounding and formatting!");
+            double d1 = 55.321678;
+            double d2 = Math.Round(d1, 2);
+            double d3 = Math.Round(d1, 4);
+            Console.WriteLine(d2);  // 55.32
+            Console.WriteLine(d3);  // 55.3217
+
+            // format d1 as a us currency
+            Console.WriteLine(d1.ToString("C"));
+        }
+    }
+}
