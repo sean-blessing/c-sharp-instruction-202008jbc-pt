8c897d4e8c9df2502acedb95a90085cabd02b45e
Sean Blessing <snblessing@gmail.com>
2020-12-02 18:27:14 -0500

    ch3 instruction

---
+++ ch03-for-loop/ch03-for-loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ch03_for_loop {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("For Loops");
+            // indexed for loop that will print "hello" n times
+            // n is an int entered by the user
+
+            Console.Write("Enter a whole number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Hello! " + i);
+            }
+        }
+    }
+}
