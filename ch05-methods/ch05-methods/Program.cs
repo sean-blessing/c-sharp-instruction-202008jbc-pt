5660f291c483e95e20fde3258b3ba0d07bd5a2bf
Sean Blessing <snblessing@gmail.com>
2020-12-02 20:32:35 -0500

    init commit

---
+++ ch05-methods/ch05-methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ch05_methods {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Call some methods!");
+            //Prompt user to enter a whole number
+            // convert the input to a number
+            // print the number
+
+            int n = GetInt("Enter a whole number:  ");
+            Console.WriteLine("You entered the number: " + n);
+
+            int age = GetInt("Enter actor age:  ");
+            Console.WriteLine("Actor's age is: " + age);
+
+            Console.WriteLine("Bye");
+        }
+
+        private static int GetInt(String prompt) {
+            int n = 0;
+            Boolean success = false;
+            while (!success) {
+                Console.Write(prompt);
+                try {
+                    n = int.Parse(Console.ReadLine());
+                    success = true;
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Invalid Entry.  Not a whole number.  Try again.");
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ ch05-optional-arguments/ch05-optional-arguments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ch05_optional_arguments {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Optional args!");
+            int nb1 = 22;
+            int nb2 = 44;
+            int nb3 = 11;
+
+            Console.WriteLine("sum2 is " + Sum(nb1, nb2));
+            Console.WriteLine("sum3 is " + Sum(nb1, nb2, nb3));
+
+
+            Console.WriteLine("Bye");
+        }
+
+        static int Sum(int n1, int n2, int n3 = 0) {
+            return n1 + n2 + n3;
+        }
+    }
+}
