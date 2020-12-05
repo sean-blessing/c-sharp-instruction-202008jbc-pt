cdd5e7700292d40bfa9ee72da47eea027c11b323
Sean Blessing <snblessing@gmail.com>
2020-12-05 15:12:46 -0500

    added tracking of # of rolls per game

--- pig-dice/pig-dice/Program.cs
+++ pig-dice/pig-dice/Program.cs
@@ -1,31 +1,43 @@
 ﻿using ch07_product_manager.util;
+using pig_dice.business;
 using System;
+using System.Collections.Generic;
 
 namespace pig_dice {
     class Program {
+        private static List<PigDiceGame> games = new List<PigDiceGame>();
+ 
         static void Main(string[] args) {
             Console.WriteLine("Pig Dice!");
             Console.WriteLine("Each game consists of die rolls until a 1 is rolled.");
             Console.WriteLine("You can play as many games as you like to try to reach the highest score.");
             int n = MyConsole.getInt("\nHow many games would you like to play?");
-            int max = 0;
+            PigDiceGame bestGame = new PigDiceGame();
+            // game loop
             for (int i=0; i< n; i++) {
+                PigDiceGame pdg = new PigDiceGame();
                 int roll = 0;
                 int total = 0;
+                int rollCount = 0;
                 while (roll!=1) {
+                    rollCount++;
                     Random rand = new Random();
                     roll = rand.Next(1, 7);
                     total += roll;
                 }
-                //Console.WriteLine("Game #" + i + " score: " + total);
-                max = Math.Max(max, total);
+                Console.WriteLine("Game #" + i + " score: " + total);
+                pdg.numRolls = rollCount;
+                pdg.totalScore = total;
+                if (total >= bestGame.totalScore)
+                    bestGame = pdg;
                 roll = 0;
                 total = 0;
             }
-            Console.WriteLine("====================");
-            Console.WriteLine("Total games:\t"+n);
-            Console.WriteLine("Max Score:\t"+max);
-            Console.WriteLine("====================");
+            Console.WriteLine("==============================");
+            Console.WriteLine("Total games:\t\t"+n);
+            Console.WriteLine("Max Score:\t\t"+bestGame.totalScore);
+            Console.WriteLine("# Rolls for Max:\t" + bestGame.numRolls);
+            Console.WriteLine("==============================");
             // other ideas
             // store # of rolls for max score?
         }
---
+++ pig-dice/pig-dice/business/PigDiceGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pig_dice.business {
+    class PigDiceGame {
+        public DateTime timestamp { get; set; }
+        public int numRolls { get; set; }
+        public int totalScore { get; set; }
+
+        public PigDiceGame() {
+            this.timestamp = new DateTime();
+            this.numRolls = 0;
+            this.totalScore = 0;
+        }
+    }
+}
